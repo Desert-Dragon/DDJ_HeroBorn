41340941c5e2a52c656eb791483b4f0d80e764c7
Desert Dragon <squirttle123@gmail.com>
2021-02-07 23:10:31 -0700

    Build V4 tested

--- Assets/script/ItemBehavior.cs
+++ Assets/script/ItemBehavior.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class ItemBehavior : MonoBehaviour
 {
+    public GameBehavior gameManager;
+
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameBehavior>();
+    }
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name == "Player")
         {
             Destroy(this.transform.parent.gameObject);
             Debug.Log("Item Collected");
-            
+            gameManager.Items += 1;
         }
     }
 }
